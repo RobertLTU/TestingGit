933f514d467710edd36ac2901d9cca85b8367f0f
RobertLTU <58054709+RobertLTU@users.noreply.github.com>
2020-04-01 23:33:39 +0300

    Testing

--- src/com/robert/Main.java
+++ src/com/robert/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good bye World");
 
         TestClass gitTestClass = new TestClass("Is this working or not ?");
 
